75a9d2c344375dad7f0265c49d916513c7a6f977
Alex Birch <scottlogic-alex@users.noreply.github.com>
2023-07-12 14:51:44 +0100

    more in-distribution prompt

--- evaluate.py
+++ evaluate.py
@@ -57,22 +57,22 @@ def smart_tokenizer_and_embedding_resize(
     tokenizer: PreTrainedTokenizer,
     model: PreTrainedModel,
 ):
-    """Resize tokenizer and embedding.
+  """Resize tokenizer and embedding.
 
-    Note: This is the unoptimized version that may make your embedding size not be divisible by 64.
-    """
-    num_new_tokens = tokenizer.add_special_tokens(special_tokens_dict)
-    model.resize_token_embeddings(len(tokenizer))
+  Note: This is the unoptimized version that may make your embedding size not be divisible by 64.
+  """
+  num_new_tokens = tokenizer.add_special_tokens(special_tokens_dict)
+  model.resize_token_embeddings(len(tokenizer))
 
-    if num_new_tokens > 0:
-        input_embeddings = model.get_input_embeddings().weight.data
-        output_embeddings = model.get_output_embeddings().weight.data
+  if num_new_tokens > 0:
+    input_embeddings = model.get_input_embeddings().weight.data
+    output_embeddings = model.get_output_embeddings().weight.data
 
-        input_embeddings_avg = input_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
-        output_embeddings_avg = output_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
+    input_embeddings_avg = input_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
+    output_embeddings_avg = output_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
 
-        input_embeddings[-num_new_tokens:] = input_embeddings_avg
-        output_embeddings[-num_new_tokens:] = output_embeddings_avg
+    input_embeddings[-num_new_tokens:] = input_embeddings_avg
+    output_embeddings[-num_new_tokens:] = output_embeddings_avg
 
 class TokenizerOutput(TypedDict):
   input_ids: LongTensor
@@ -327,9 +327,10 @@ def main():
       format_message(message) for message in [
         *history,
         Message(Participant.User, user_input),
-        Message(Participant.Assistant, ''),
+        Message(Participant.Assistant, process_supervision_tokens['step_start']),
       ]
     ])
+    chat_to_complete: str = f'{tokenizer.bos_token}{chat_to_complete}'
 
     tokenized_prompts: TokenizerOutput = tokenizer([chat_to_complete], return_tensors='pt', truncation=True)
     
