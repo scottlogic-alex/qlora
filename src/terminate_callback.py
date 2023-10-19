ddacd5e718785b98ae2ee6a6cb8368bc7516cc79
Alex Birch <scottlogic-alex@users.noreply.github.com>
2023-10-19 17:23:22 +0100

    on exit, print some details about some of the hyperparams that influence model memory usage

--- src/terminate_callback.py
+++ src/terminate_callback.py
@@ -7,4 +7,13 @@ class TerminateCallback(TrainerCallback):
     def on_step_end(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
         if state.global_step >= self.final_step:
             print(f'Reached step {self.final_step}; terminating due to TerminateCallback')
+            # note: does not consider possibility of mixed-precision fp8. I can live with that.
+            uses_half_precision = (args.fp16 or args.bf16) and args.half_precision_backend in ('auto', 'cuda')
+            half_precision_dtype = 'float16' if args.fp16 else 'bfloat16'
+            precision = f'mixed ({half_precision_dtype})' if uses_half_precision else 'uniform'
+            print(f'''model:     {kwargs['model'].config._name_or_path}
+precision: {precision}
+grad acc:  {args.gradient_accumulation_steps}
+grad ckpt: {args.gradient_checkpointing}
+optim:     {args.optim.value}''')
             raise SystemExit(0)
