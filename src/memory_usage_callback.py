f83c074b16309eb061288c11056a5998373c9a10
Alex Birch <scottlogic-alex@users.noreply.github.com>
2023-10-19 14:44:55 +0100

    brief memory usage printer

--- src/memory_usage_callback.py
+++ src/memory_usage_callback.py
@@ -1,9 +1,13 @@
+from __future__ import annotations
 # https://pypi.org/project/nvidia-ml-py/
 # pip install nvidia-ml-py
 import pynvml as nvml
 from transformers import TrainerCallback, TrainingArguments, TrainerState, TrainerControl
-from typing import NamedTuple, List
+from typing import NamedTuple, List, Callable
 import torch
+
+def justify(bytes: int) -> str:
+    return str(bytes).rjust(5)
 
 def to_MiB(bytes: int) -> int:
     return bytes >> 20
@@ -28,7 +32,8 @@ def torch_memory_usage(device=0) -> TorchMemoryStats:
 class MemoryUsageCallback(TrainerCallback):
     visible_nvml_device_ixs: List[int]
     substep: int
-    def __init__(self) -> None:
+    make_memory_str: Callable[[MemoryUsageCallback, str], str]
+    def __init__(self, brief=True) -> None:
         super().__init__()
         nvml.nvmlInit()
         device_count: int = nvml.nvmlDeviceGetCount()
@@ -39,11 +44,12 @@ class MemoryUsageCallback(TrainerCallback):
         # so cuda:0 will correspond to nvml device 1.
         self.visible_nvml_device_ixs = [torch.cuda._get_nvml_device_index(ix) for ix in range(torch.cuda.device_count())]
         self.substep = 0
+        self.make_memory_str = self.make_memory_str_brief if brief else self.make_memory_str_
 
-    def print_memory(self, qualifier: str):
+    def make_memory_str_(self, qualifier: str) -> str:
         overall_nvml_used = 0
         overall_nvml_total = 0
-        print(f'{qualifier}\n  NVML memory stats (used+reserved, all processes):')
+        out = f'{qualifier}\n  NVML memory stats (used+reserved, all processes):'
         # for did, handle in enumerate(self.handles):
         for nvml_did in self.visible_nvml_device_ixs:
             nvml_handle = self.handles[nvml_did]
@@ -54,13 +60,13 @@ class MemoryUsageCallback(TrainerCallback):
             # that's because it's used + reserved.
             # you can compute used+reserved via nvidia-smi yourself:
             # nvidia-smi -i 0 -q -d MEMORY
-            print(f'    Device {nvml_did}: Used {to_MiB(used_bytes)} MiB / {to_MiB(total_bytes)} MiB')
+            out += f'\n    Device {nvml_did}: Used {to_MiB(used_bytes)} MiB / {to_MiB(total_bytes)} MiB'
         if len(self.visible_nvml_device_ixs) > 1:
-            print(f'    Overall: Used {to_MiB(overall_nvml_used)} MiB / {to_MiB(overall_nvml_total)} MiB')
+            out += f'\n    Overall: Used {to_MiB(overall_nvml_used)} MiB / {to_MiB(overall_nvml_total)} MiB'
 
         overall_torch_used = 0
         overall_torch_used_plus_reserved_bytes = 0
-        print('  Torch memory stats (allocated, reserved):')
+        out += '\n  Torch memory stats (allocated, reserved):'
         for torch_did in range(torch.cuda.device_count()):
             nvml_did: int = self.visible_nvml_device_ixs[torch_did]
             used_bytes, used_plus_reserved_bytes = torch_memory_usage(torch_did)
@@ -68,13 +74,40 @@ class MemoryUsageCallback(TrainerCallback):
             overall_torch_used_plus_reserved_bytes += used_plus_reserved_bytes
             # Allocated/resident includes stuff like optimizer state
             # Reserved includes temporary state like gradients
-            print(f'    Device {nvml_did}: Used {to_MiB(used_plus_reserved_bytes)} MiB (Allocated: {to_MiB(used_bytes)} MiB, Reserved {to_MiB(used_plus_reserved_bytes-used_bytes)} MiB)')
+            out += f'\n    Device {nvml_did}: Used {to_MiB(used_plus_reserved_bytes)} MiB (Allocated: {to_MiB(used_bytes)} MiB, Reserved {to_MiB(used_plus_reserved_bytes-used_bytes)} MiB)'
         if len(self.visible_nvml_device_ixs) > 1:
-            print(f'    Overall: Used {to_MiB(overall_torch_used_plus_reserved_bytes)} MiB (Allocated: {to_MiB(overall_torch_used)} MiB, Reserved {to_MiB(overall_torch_used_plus_reserved_bytes-overall_torch_used)} MiB)')
+            out += f'\n    Overall: Used {to_MiB(overall_torch_used_plus_reserved_bytes)} MiB (Allocated: {to_MiB(overall_torch_used)} MiB, Reserved {to_MiB(overall_torch_used_plus_reserved_bytes-overall_torch_used)} MiB)'
+        return out
+    
+    def make_memory_str_brief(self, qualifier: str) -> str:
+        spacer: str = ''.rjust(len(qualifier))
+        overall_torch_used_bytes = 0
+        overall_torch_used_plus_reserved_bytes = 0
+        overall_nvml_used_bytes = 0
+        overall_nvml_total_bytes = 0
+        lines: List[str] = []
+        for torch_did in range(torch.cuda.device_count()):
+            nvml_did: int = self.visible_nvml_device_ixs[torch_did]
+            nvml_handle = self.handles[nvml_did]
+            nvml_used_bytes, nvml_total_bytes = nvml_memory_usage(nvml_handle)
+            overall_nvml_used_bytes += nvml_used_bytes
+            overall_nvml_total_bytes += nvml_total_bytes
+            torch_used_bytes, torch_used_plus_reserved_bytes = torch_memory_usage(torch_did)
+            overall_torch_used_bytes += torch_used_bytes
+            overall_torch_used_plus_reserved_bytes += torch_used_plus_reserved_bytes
+            lines.append(f'Device {nvml_did}: Torch Used {justify(to_MiB(torch_used_plus_reserved_bytes))} MiB (Allocated: {justify(to_MiB(torch_used_bytes))} MiB, Reserved {justify(to_MiB(torch_used_plus_reserved_bytes-torch_used_bytes))} MiB), NVML {justify(to_MiB(nvml_used_bytes))} / {justify(to_MiB(nvml_total_bytes))} MiB')
+        if len(self.visible_nvml_device_ixs) > 1:
+            lines.append(f'Overall:  Torch Used {justify(to_MiB(overall_torch_used_plus_reserved_bytes))} MiB (Allocated: {justify(to_MiB(overall_torch_used_bytes))} MiB, Reserved {justify(to_MiB(overall_torch_used_plus_reserved_bytes-overall_torch_used_bytes))} MiB), NVML {justify(to_MiB(overall_nvml_used_bytes))} / {justify(to_MiB(overall_nvml_total_bytes))} MiB')
+        first, *rest = lines
+        out: str = '\n'.join((
+            f'{qualifier} {first}',
+            *(f'{spacer} {line}' for line in rest)
+        ))
+        return out
     
     def on_substep_end(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
         torch.cuda.synchronize()
-        self.print_memory(f'step {state.global_step}, microstep {self.substep}')
+        print(self.make_memory_str(f'step {state.global_step}, microstep {self.substep}'))
         self.substep += 1
     
     def on_step_begin(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
@@ -84,6 +117,6 @@ class MemoryUsageCallback(TrainerCallback):
         torch.cuda.synchronize()
         microstep_detail = '(end)'.rjust(13) if self.substep else ''
         qualifier = f'step {state.global_step - 1}{microstep_detail}'
-        self.print_memory(qualifier)
+        print(self.make_memory_str(qualifier))
         
 
