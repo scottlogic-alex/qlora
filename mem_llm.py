e92961bafe76627bffb8b080591a5614b6a33a3a
Alex Birch <scottlogic-alex@users.noreply.github.com>
2023-10-23 15:12:47 +0000

    DDP support

--- mem_llm.py
+++ mem_llm.py
@@ -3,11 +3,29 @@ import torch
 from torch import BoolTensor, LongTensor
 from torch.cuda.amp import autocast
 from torch.optim import SGD
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
 from contextlib import nullcontext
 from transformers import AutoModelForCausalLM, PreTrainedModel, AutoConfig, PretrainedConfig#, PreTrainedTokenizer, PreTrainedTokenizerFast, AutoTokenizer
 from transformers.modeling_outputs import CausalLMOutputWithPast
 import argparse
 from typing import List, NamedTuple#, Union
+from logging import getLogger, Formatter, StreamHandler
+import logging
+
+logger = getLogger(__file__)
+log_fmt_nominal = '{message}'
+
+rank0_logger = logger.getChild('rank0')
+rank0_logger.setLevel(logging.INFO)
+rank0_handler = StreamHandler()
+rank0_handler.setFormatter(Formatter(fmt=log_fmt_nominal, style='{'))
+
+all_logger = logger.getChild('all')
+all_logger.setLevel(logging.INFO)
+all_handler = StreamHandler()
+all_handler.setFormatter(Formatter(fmt=log_fmt_nominal, style='{'))
+all_logger.addHandler(all_handler)
 
 class TorchCudaMemoryBytes(NamedTuple):
   alloc: int
@@ -36,7 +54,7 @@ def mem_summary(mem_metric: TorchCudaMemoryBytes, params: int) -> str:
   bytes_per_param = alloc_plus_reserved/params
   return f'{mib_str(alloc_plus_reserved)}; {f"{bytes_per_param:.2f}".rjust(5)} bytes/param (of which {mib_str(alloc)} alloc, {mib_str(reserved)} reserved)'
 
-def mem(preamble: str, params: int, multi_device: bool):
+def mem(preamble: str, params: int, multi_device: bool, device_id: int):
   if multi_device and torch.cuda.device_count() > 1:
     out_lines: List[str] = []
     total_metric = TorchCudaMemoryBytes(0, 0)
@@ -51,7 +69,7 @@ def mem(preamble: str, params: int, multi_device: bool):
     out_lines.append(f'{once_preamble}  =: {total_summary}')
     return '\n'.join(out_lines)
   else:
-    mem_metric: TorchCudaMemoryBytes = device_mem()
+    mem_metric: TorchCudaMemoryBytes = device_mem(device_id)
     summary: str = mem_summary(mem_metric, params)
     return f'{preamble}: {summary}'
 
@@ -59,7 +77,7 @@ def main():
   parser = argparse.ArgumentParser(prog='LLM finetuning memory measurer')
   parser.add_argument('--model_name', type=str, default='EleutherAI/pythia-70m-deduped')
   parser.add_argument('--cache_dir', type=str, default=None)
-  parser.add_argument('--batch_size', type=int, default=1)
+  parser.add_argument('--batch_size', type=int, default=1, help='per-process batch size')
   parser.add_argument('--seq_len', type=int, default=8)
   parser.add_argument('--grad_ckpt', action='store_true')
   parser.add_argument('--steps', type=int, default=1)
@@ -67,15 +85,35 @@ def main():
   parser.add_argument('--device_map_auto', action='store_true')
   parser.add_argument('--skip_ckpt_load', action='store_true')
   parser.add_argument('--mixed_bf16', action='store_true')
+  # if we run distributed: torchrun passes this option in. we use dist.get_rank() instead, to stick closer to the torch distributed tutorial.
+  parser.add_argument('--local-rank', type=int, default=None)
   args = parser.parse_args()
 
-  print(f'''grad acc {'en' if args.microsteps > 1 else 'dis'}abled
-precision: {'mixed' if args.mixed_bf16 else 'uniform'}''')
+  is_distributed: bool = args.local_rank is not None
+  if is_distributed:
+    assert not args.device_map_auto, "Have not implemented support for device_map='auto' in distributed mode"
+    dist.init_process_group("nccl")
+    rank: int = dist.get_rank()
+    all_handler.setFormatter(Formatter(fmt=f'r{rank} {log_fmt_nominal}', style='{'))
+    rank0_handler.setFormatter(Formatter(fmt=f'   {log_fmt_nominal}', style='{'))
+  else:
+    rank = 0
+  if rank == 0:
+    rank0_logger.addHandler(rank0_handler)
+    rank0_logger.setLevel(logging.INFO)
+  else:
+    rank0_logger.setLevel(logging.ERROR)
+  
+  device_id: int = rank % torch.cuda.device_count()
+  device = torch.device(device_id)
+
+  rank0_logger.info(f"grad acc:  {'en' if args.microsteps > 1 else 'dis'}abled")
+  rank0_logger.info(f"precision: {'mixed' if args.mixed_bf16 else 'uniform'}")
 
   realloc_each_microstep = True
   optim_set_to_none = True
 
-  device=torch.device('cuda')
+  device=torch.device('cuda', device_id)
 
   if args.skip_ckpt_load:
     assert not args.device_map_auto, "for some reason device_map='auto' support isn't implemented for AutoModelForCausalLM#from_config()"
@@ -91,7 +129,7 @@ precision: {'mixed' if args.mixed_bf16 else 'uniform'}''')
       device_map='auto' if args.device_map_auto else None,
     )
   param_count = sum([p.numel() for p in model.parameters()])
-  print('param count', param_count)
+  rank0_logger.info(f'param count: {param_count}')
 
   # tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast] = AutoTokenizer.from_pretrained(
   #   args.model_name,
@@ -111,6 +149,11 @@ precision: {'mixed' if args.mixed_bf16 else 'uniform'}''')
   # model.train()
   # model.zero_grad()
 
+  vocab_size: int = model.config.vocab_size
+
+  if is_distributed:
+    model = DDP(model, device_ids=[device_id])
+
   optim = SGD(model.parameters(), lr=2e-5, momentum=0.)
 
   precision_ctx = autocast(dtype=torch.bfloat16, cache_enabled=True) if args.mixed_bf16 else nullcontext()
@@ -124,7 +167,7 @@ precision: {'mixed' if args.mixed_bf16 else 'uniform'}''')
       microstep_indicator = f'[microstep {microstep}] ' if args.microsteps > 1 else microstep_indicator_padding
       step_and_micro_indicator = f'{step_indicator}{microstep_indicator}'
       if realloc_each_microstep or step == 0 and microstep == 0:
-        input_ids: LongTensor = torch.randint(model.config.vocab_size, (args.batch_size, args.seq_len), device=device, requires_grad=False)
+        input_ids: LongTensor = torch.randint(vocab_size, (args.batch_size, args.seq_len), device=device, requires_grad=False)
         labels: LongTensor = input_ids.clone()
         labels[:,:4] = -100
         attention_mask: BoolTensor = torch.ones_like(input_ids, dtype=torch.bool)
@@ -142,11 +185,16 @@ precision: {'mixed' if args.mixed_bf16 else 'uniform'}''')
       model_out.loss.backward()
       del model_out
 
-      print(mem(f'{step_and_micro_indicator}after loss backward', params=param_count, multi_device=args.device_map_auto))
+      all_logger.info(mem(f'{step_and_micro_indicator}after loss backward', params=param_count, multi_device=args.device_map_auto, device_id=device_id))
 
     optim.step()
     optim.zero_grad(set_to_none=optim_set_to_none)
-    print(mem(f'{step_indicator}{microstep_indicator_padding}after  zero_grad())', params=param_count, multi_device=args.device_map_auto))
+    all_logger.info(mem(f'{step_indicator}{microstep_indicator_padding}after  zero_grad())', params=param_count, multi_device=args.device_map_auto, device_id=device_id))
+
+    if is_distributed:
+      # the tutorials says we should destroy our process group, but when I tried this it complained that there was no process group.
+      # dist.destroy_process_group()
+      pass
   
 if __name__ == "__main__":
   main()
